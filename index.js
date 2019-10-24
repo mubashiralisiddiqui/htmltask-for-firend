c68bcd8bc39249ed68e854684b7c30d84d5314c8
MubahirAli <mubahir22ali@gmail.com>
2019-10-25 00:21:08 +0500

    variable declear

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 
-var BTN1 = 0;
-var BTN2 = 0;
-var BTN3 = 0;
-var BTN4 = 0;
-var Delivery = 0;
-var TakeoutOrDelivery = 'no';
-var total = 0;
+var cheese = document.getElementById('cheese')
+var mushroom = document.getElementById('mushroom')
+var sausage = document.getElementById('sausage')
+var veggie = document.getElementById('veggie')
+var takeOut = document.getElementById('takOut')
+var delivery = document.getElementById('delivery')
+
+
+var fifteenTip = document.getElementById('tip15')
+var eighteenTip = document.getElementById('tip18')
+var twentyTip = document.getElementById('tip20')
+var noTip = document.getElementById('notip')
+
+
+
+
+function handleSubmit() {
+
+    // jo calculation karni he yaha karo in varible ko use karte hue 
+    console.log(cheese.value)
+    console.log(mushroom.value)
+    console.log(veggie.value)
+    console.log(sausage.value)
+    console.log(takeOut.checked)
+    console.log(delivery.checked)
+
+
+
+    console.log('fifteenTip', fifteenTip.checked)
+    console.log("eighteenTip", eighteenTip.checked)
+    console.log("twentyTip", twentyTip.checked)
+    console.log("noTip", noTip.checked)
+
+
+
+
+
+
+
+
+
+
+
+}
